334d9499a98a48fb81f7f59eac30bacefff3d362
Guilherme Shizuru <141662172+guishizuru@users.noreply.github.com>
2026-03-24 11:14:12 -0300

    feature - criacao de entidade repository payment

---
+++ src/main/java/com/br/payFlow/repository/PaymentRepository.java
@@ -0,0 +1,14 @@
+package com.br.payFlow.repository;
+
+import com.br.payFlow.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface PaymentRepository  extends JpaRepository<Payment, UUID> {
+
+    Optional<Payment> findByExternalId(String externalId);
+}

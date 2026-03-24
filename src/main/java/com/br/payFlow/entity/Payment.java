aa03e102ebc8aa814108860507219aa531db1e65
Guilherme Shizuru <141662172+guishizuru@users.noreply.github.com>
2026-03-24 11:13:47 -0300

    feature - criacao de entidade payment com idempontencia

---
+++ src/main/java/com/br/payFlow/entity/Payment.java
@@ -0,0 +1,41 @@
+package com.br.payFlow.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(name = "external_id", unique = true)
+    private String externalId;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}

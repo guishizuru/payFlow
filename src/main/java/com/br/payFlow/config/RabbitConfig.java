3be05440b5aa26babe4edaa6da4ffe9f6ed61806
Guilherme Shizuru <141662172+guishizuru@users.noreply.github.com>
2026-03-24 19:11:44 -0300

    feature - criacao de metodo para o DLQ

--- src/main/java/com/br/payFlow/config/RabbitConfig.java
+++ src/main/java/com/br/payFlow/config/RabbitConfig.java
@@ -1,6 +1,6 @@
 package com.br.payFlow.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +8,28 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     public static final String PAYMENT_QUEUE = "payment.queue";
-
+    public static final String PAYMENT_DLQ = "payment.dlq";
+    public static final String PAYMENT_DLX = "payment.dlx";
     @Bean
     public Queue paymentQueue() {
-        return new Queue(PAYMENT_QUEUE, true);
+        return QueueBuilder.durable(PAYMENT_QUEUE)
+                .withArgument("x-dead-letter-exchange", PAYMENT_DLX)
+                .build();
+    }
+    @Bean
+    public Queue paymentDLQ() {
+        return new Queue(PAYMENT_DLQ);
+    }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(PAYMENT_DLX);
+    }
+
+    @Bean
+    public Binding dlqBinding() {
+        return BindingBuilder.bind(paymentDLQ())
+                .to(deadLetterExchange())
+                .with(PAYMENT_QUEUE);
     }
 }

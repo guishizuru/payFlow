439efa56287928dac1e30c56546194b15549dc0b
Guilherme Shizuru <141662172+guishizuru@users.noreply.github.com>
2026-03-26 13:49:55 -0300

    refactor - insercao de @AllArgsConstructor

--- src/main/java/com/br/payFlow/dto/request/PaymentRequestDTO.java
+++ src/main/java/com/br/payFlow/dto/request/PaymentRequestDTO.java
@@ -3,11 +3,13 @@ package com.br.payFlow.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class PaymentRequestDTO {
 
     @NotNull
